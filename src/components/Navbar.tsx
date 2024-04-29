5c543a4154d1458043d49c6eadc014ba4231765c
BlackMoonC <rakelsera2001@gmail.com>
2024-04-29 15:47:16 +0700

    deploy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 export default function Navbar() {
   const navigate = useNavigate();
   const removeUser = () => {
